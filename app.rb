3415180ceb14cdd7813cae209e01c5d210331577
Edward <103263530+edra-dev@users.noreply.github.com>
2022-05-31 17:04:08 +0100

    Use a redirect as names

--- app.rb
+++ app.rb
@@ -1,30 +1,33 @@
-require 'sinatra/base'
-require 'sinatra/reloader'
+require "sinatra/base"
+require "sinatra/reloader"
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   configure :development do
     register Sinatra::Reloader
   end
 
-  get '/test' do
+  get "/test" do
     "Testing infrastructure working!"
   end
 
-  get '/' do 
-    erb(:name_form)
-  end
-
-  get "/name_cat" do
+  get "/" do
     erb(:index)
   end
 
-  post '/start' do
+  get "/play" do
+    @player_1 = session[:Player_1]
+    @player_2 = session[:Player_2]
+    erb(:play)
+  end
+
+  post "/names" do
     p params
-    @player_1 = params[:Player_1]
-    @player_2 = params[:Player_2]
-    erb(:start)
+    session[:Player_1] = params[:Player_1]
+    session[:Player_2] = params[:Player_2]
+    redirect ("/play")
   end
 
   run! if app_file == $0
 end
-
--- spec/features/Player_names_spec.rb
+++ spec/features/Player_names_spec.rb
@@ -1,13 +1,11 @@
 RSpec.describe "player name form" do
-
-  feature 'form for player names' do
-    scenario 'players can enter their names and see them' do
-      visit ('/')
-      fill_in 'Player_1', with: "Annabelle"
-      fill_in 'Player_2', with: "Bella"
-      click_on 'Submit'
+  feature "form for player names" do
+    scenario "players can enter their names and see them" do
+      visit ("/")
+      fill_in "Player_1", with: "Annabelle"
+      fill_in "Player_2", with: "Bella"
+      click_on "Submit"
       expect(page).to have_content "Player 1: Annabelle,\nPlayer 2: Bella"
     end
   end
-
 end
