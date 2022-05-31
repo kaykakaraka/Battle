df4b1a7694ebdfe8177cf7a4044c81b2d011582f
Edward <103263530+edra-dev@users.noreply.github.com>
2022-05-31 17:25:47 +0100

    Viewing hit points (partially complete)

--- spec/features/see_points_spec.rb
+++ spec/features/see_points_spec.rb
@@ -1,10 +1,11 @@
-RSpec.describe "player name form" do
-  feature "form for player names" do
-    scenario "players can enter their names and see them" do
+RSpec.describe "To show player_2's points" do
+  feature "the score" do
+    scenario "is displayed" do
       visit ("/")
       fill_in "Player_1", with: "Annabelle"
       fill_in "Player_2", with: "Bella"
       click_on "Submit"
+
       expect(page).to have_content "Player 1: Annabelle has 100 points\nPlayer 2: Bella has 100 points"
     end
   end
